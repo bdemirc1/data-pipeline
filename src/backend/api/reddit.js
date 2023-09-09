9ae8083094e0ccd82b889906c35e7a355a0df785
Busra Demirci <bdemirc1@binghamton.edu>
2023-09-08 22:28:41 -0400

    backend fetch

--- src/backend/api/reddit.js
+++ src/backend/api/reddit.js
@@ -1,10 +1,7 @@
-export const API_ROOT = 'https://www.reddit.com';
 
 export const getsubReddits = async () => {
-  const response = await fetch(`${API_ROOT}/subreddits.json`);
-  const json = await response.json();
-  console.log(json.data.children);
-  return json.data.children.map(child => child.data);
-};
-
-
+    const response = await fetch("/subreddits");
+    const json = await response.json();
+    console.log(json.data.children);
+    return json.data.children.map(child => child.data);
+  };
